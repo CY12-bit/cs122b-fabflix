ce6597c267d94564f667f8f6323baa2fd0e58898
Colin <68120455+CY12-bit@users.noreply.github.com>
2023-10-31 10:34:45 -0700

    Created add_movie stored procedure

---
+++ add_movie.sql
@@ -0,0 +1,34 @@
+USE moviedb;
+
+DROP PROCEDURE IF EXISTS add_movie;
+DELIMITER $$
+CREATE PROCEDURE add_movie(
+    IN mTitle VARCHAR(100),
+    IN mYear INTEGER,
+    IN mDirector VARCHAR(100),
+    IN price FLOAT,
+    IN star_name VARCHAR(100),
+    IN genre_name VARCHAR(32)
+)
+BEGIN
+IF (mTitle,mYear,mDirector) NOT IN (SELECT title,year,director FROM movies) THEN 	# If the movie does not exist
+    INSERT into movies 
+    SELECT CONCAT('tt',LPAD(CAST((MAX(CAST(SUBSTRING(id, 3) AS unsigned)) + 1) AS CHAR(10)),7,'0')),
+    mTitle,mYear,mDirector,price FROM movies; 
+    IF star_name NOT IN (SELECT DISTINCT name FROM stars) THEN
+        INSERT INTO stars(id,name,birthYear) 
+        SELECT CONCAT('nm',LPAD(CAST((MAX(CAST(SUBSTRING(id, 3) AS unsigned)) + 1) AS CHAR(10)),7,'0')),
+		star_name, NULL
+		FROM stars;
+    END IF;
+    IF genre_name NOT IN (SELECT DISTINCT name FROM genres) THEN
+        INSERT INTO genres(name) VALUES (genre_name);
+    END IF;
+    SELECT id INTO @movie_id FROM movies WHERE title = mTitle AND year = mYear AND director = mDirector;
+    SELECT id INTO @star_id FROM stars WHERE name = star_name;
+    SELECT id INTO @genre_id FROM genres WHERE name = genre_name;
+    INSERT INTO stars_in_movies(starId,movieId) VALUES (@star_id,@movie_id);
+    INSERT INTO genres_in_movies(genreId, movieId) VALUES (@genre_id,@movie_id);
+END IF;
+END $$
+DELIMITER ;
