5c8ad6d68aff280b618ce53238e83de50b77854c
Colin <68120455+CY12-bit@users.noreply.github.com>
2023-11-01 22:54:33 -0700

    Added the employee table

--- src/Employee/DashboardServlet.java
+++ src/Employee/DashboardServlet.java
@@ -10,6 +10,7 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "Employee.DashboardServlet", urlPatterns = "/api/dashboard")
 public class DashboardServlet extends HttpServlet {
