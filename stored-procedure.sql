5d1c40f9a432442b365e5ef4d0ff542c788a7c26
michellelin1 <michellelin810@gmail.com>
2023-11-09 00:35:21 -0800

    optimized query and add_star procedure

--- stored-procedure.sql
+++ stored-procedure.sql
@@ -10,17 +10,17 @@ CREATE PROCEDURE add_movie(
     IN genre_name VARCHAR(32)
 )
 BEGIN
-IF (mTitle,mYear,mDirector) NOT IN (SELECT title,year,director FROM movies) THEN 	# If the movie does not exist
+IF (SELECT NOT EXISTS(SELECT 1 FROM movies WHERE mTitle = title AND mYear = year AND mDirector = director)) THEN 	# If the movie does not exist
     INSERT into movies 
     SELECT CONCAT('tt',LPAD(CAST((MAX(CAST(SUBSTRING(id, 3) AS unsigned)) + 1) AS CHAR(10)),7,'0')),
     mTitle,mYear,mDirector,FLOOR(RAND()*(10-5+1)+5) FROM movies; 
-    IF star_name NOT IN (SELECT DISTINCT name FROM stars) THEN
+    IF (SELECT NOT EXISTS(SELECT 1 FROM stars WHERE star_name = name)) THEN
         INSERT INTO stars(id,name,birthYear) 
         SELECT CONCAT('nm',LPAD(CAST((MAX(CAST(SUBSTRING(id, 3) AS unsigned)) + 1) AS CHAR(10)),7,'0')),
 		star_name, NULL
 		FROM stars;
     END IF;
-    IF genre_name NOT IN (SELECT DISTINCT name FROM genres) THEN
+    IF (SELECT NOT EXISTS(SELECT 1 FROM genres WHERE genre_name = name)) THEN
         INSERT INTO genres(name) VALUES (genre_name);
     END IF;
     SELECT id INTO @movie_id FROM movies WHERE title = mTitle AND year = mYear AND director = mDirector;
@@ -28,7 +28,9 @@ IF (mTitle,mYear,mDirector) NOT IN (SELECT title,year,director FROM movies) THEN
     SELECT id INTO @genre_id FROM genres WHERE name = genre_name;
     INSERT INTO stars_in_movies(starId,movieId) VALUES (@star_id,@movie_id);
     INSERT INTO genres_in_movies(genreId, movieId) VALUES (@genre_id,@movie_id);
-	SELECT @movie_id AS movie_id, @star_id AS star_id, @genre_id AS genre_id;
+    SELECT @movie_id AS movie_id, @star_id AS star_id, @genre_id AS genre_id;
+ELSE
+	SELECT null AS movie_id, null AS star_id, null AS genre_id;
 END IF;
 END $$
 DELIMITER ;
@@ -40,13 +42,10 @@ CREATE PROCEDURE add_star(
     IN birth_year INTEGER
 )
 BEGIN
-    IF (star_name, birth_year) NOT IN (SELECT name, birthYear FROM stars) THEN
-        INSERT INTO stars(id,name,birthYear)
-        SELECT CONCAT('nm',LPAD(CAST((MAX(CAST(SUBSTRING(id, 3) AS unsigned)) + 1) AS CHAR(10)),7,'0')),
-               star_name, birth_year
-        FROM stars;
+    DECLARE maxId INTEGER;
+    SELECT CAST(SUBSTR(id, 3) AS UNSIGNED) INTO maxId FROM stars ORDER BY CAST(SUBSTR(id, 3) AS UNSIGNED) DESC LIMIT 1;
+    INSERT INTO stars(id,name,birthYear) VALUES (CONCAT('nm', maxId+1),star_name, birth_year);
 
-        SELECT id AS star_id FROM stars WHERE name = star_name AND birthYear = birth_year;
-    END IF;
+    SELECT CONCAT('nm', maxId+1);
 END $$
 DELIMITER ;
