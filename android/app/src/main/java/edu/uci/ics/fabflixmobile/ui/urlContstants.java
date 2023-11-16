c8864bbb9b2062bc5ed0f38c091fc373e95f88ca
Colin <68120455+CY12-bit@users.noreply.github.com>
2023-11-16 14:56:38 -0800

    Create urlContstants.java

---
+++ android/app/src/main/java/edu/uci/ics/fabflixmobile/ui/urlContstants.java
@@ -0,0 +1,8 @@
+package edu.uci.ics.fabflixmobile.ui;
+
+public class urlContstants {
+    private static final String host = "10.0.2.2";
+    private static final String port = "8080";
+    private static final String domain = "cs122b_projects_war";
+    public static final String baseURL = "http://" + host + ":" + port + "/" + domain;
+}
