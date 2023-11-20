aa9b10cd511ed29139e0589d14c072d77caa234f
Colin <68120455+CY12-bit@users.noreply.github.com>
2023-11-19 17:59:29 -0800

    Updated urlConstrants for AWS

--- android/app/src/main/java/edu/uci/ics/fabflixmobile/ui/urlContstants.java
+++ android/app/src/main/java/edu/uci/ics/fabflixmobile/ui/urlContstants.java
@@ -1,8 +1,8 @@
 package edu.uci.ics.fabflixmobile.ui;
 
 public class urlContstants {
-    private static final String host = "10.0.2.2";
+    private static final String host = "3.101.74.79"; //  10.0.2.2 is for localhost
     private static final String port = "8443";
-    private static final String domain = "cs122b_projects_war";
+    private static final String domain = "cs122b-projects"; // cs122b_projects_war is for localhost
     public static final String baseURL = "https://" + host + ":" + port + "/" + domain;
 }
