c8b1b8581758293e5200e23451fb6b038e54ee51
Colin <68120455+CY12-bit@users.noreply.github.com>
2023-11-28 13:49:46 -0800

    changed hosts to local. Added ConnectionPooling to context.xml

--- android/app/src/main/java/edu/uci/ics/fabflixmobile/ui/urlConstants.java
+++ android/app/src/main/java/edu/uci/ics/fabflixmobile/ui/urlConstants.java
@@ -1,8 +1,8 @@
 package edu.uci.ics.fabflixmobile.ui;
 
 public class urlConstants {
-    private static final String host = "3.101.61.30"; //  10.0.2.2 is for localhost
+    private static final String host = "10.0.2.2"; //  10.0.2.2 is for localhost
     private static final String port = "8443";
-    private static final String domain = "cs122b-projects"; // cs122b_projects_war is for localhost. cs122b-projects for AWS
+    private static final String domain = "cs122b_projects_war"; // cs122b_projects_war is for localhost. cs122b-projects for AWS
     public static final String baseURL = "https://" + host + ":" + port + "/" + domain;
 }
